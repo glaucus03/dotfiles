fa0e33be80f0650cd1610c53a965a2f68d1af4cc
glaucus03 <glaucus03.tech@gmail.com>
2023-11-23 17:46:32 +0900

    feat: add kitty :tada:

--- scripts/create_symboliclink.sh
+++ scripts/create_symboliclink.sh
@@ -1,4 +1,5 @@
 cd dotfiles dir
 ln -sf $(pwd)/.vim ~/.vim
 ln -sf $(pwd)/.config/wezterm ~/.config/wezterm
+ln -sf $(pwd)/.config/kitty ~/.config/kitty
 ln -sf $(pwd)/.config/starship.toml ~/.config/starship.toml
