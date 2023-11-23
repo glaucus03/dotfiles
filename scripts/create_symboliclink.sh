111d5b5815906992a02538a06932d073c16110db
glaucus03 <glaucus03.tech@gmail.com>
2023-11-23 20:48:21 +0900

    feat: add lazygit:tada:

--- scripts/create_symboliclink.sh
+++ scripts/create_symboliclink.sh
@@ -2,4 +2,5 @@ cd dotfiles dir
 ln -sf $(pwd)/.vim ~/.vim
 ln -sf $(pwd)/.config/wezterm ~/.config/wezterm
 ln -sf $(pwd)/.config/kitty ~/.config/kitty
+ln -sf $(pwd)/.config/lazygit ~/.config/lazygit
 ln -sf $(pwd)/.config/starship.toml ~/.config/starship.toml
