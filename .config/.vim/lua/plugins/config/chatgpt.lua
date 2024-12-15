223bbdec8927f18eb78e7ef2fb72a5fed2cbc79d
glaucus03 <glaucus03.tech@gmail.com>
2024-12-15 14:37:27 +0900

    feat: add secrets :sparkles:

--- .config/.vim/lua/plugins/config/chatgpt.lua
+++ .config/.vim/lua/plugins/config/chatgpt.lua
@@ -1,3 +1,5 @@
+local home = vim.fn.expand("$HOME")
 require("chatgpt").setup({
-  actions_paths = 'chatgpt_actions.json'
+    api_key_cmd = "gpg --decrypt " .. home .. "/secret.txt.gpg",
+    actions_paths = {"~/dev/projects/dotfiles/.config/.vim/lua/plugins/config/chatgpt_actions.json"}
 })
