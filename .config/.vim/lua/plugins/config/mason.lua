1468cca516b2e67adbc372b83647b94d0bf270c1
glaucus03 <glaucus03.tech@gmail.com>
2025-02-11 15:15:33 +0900

    fix: fix mason settings :+1:

--- .config/.vim/lua/plugins/config/mason.lua
+++ .config/.vim/lua/plugins/config/mason.lua
@@ -1,3 +1,27 @@
+local lspconfig = require("lspconfig")
+local nlspsettings = require("nlspsettings")
+
+nlspsettings.setup({
+  config_home = vim.fn.stdpath('config') .. '/nlsp-settings',
+  local_settings_dir = ".nlsp-settings",
+  local_settings_root_markers_fallback = { '.git' },
+  append_default_schemas = true,
+  loader = 'json'
+})
+
+function on_attach(client, bufnr)
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+
+local global_capabilities = vim.lsp.protocol.make_client_capabilities()
+global_capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+  capabilities = global_capabilities,
+})
+
+
 require('mason').setup {
   ui = {
     check_outdated_packages_on_open = false,
@@ -5,9 +29,30 @@ require('mason').setup {
   },
 }
 
--- ここに追記
+require("mason-lspconfig").setup {
+	ensure_installed = {
+			"lua_ls",
+			"bashls",
+			"clangd",
+			"cmake",
+			"cssls",
+			"dockerls",
+			"docker_compose_language_service",
+			"gopls",
+			"html",
+			"jsonls",
+			"tsserver",
+			"marksman",
+			"nimls",
+			"pylsp",
+			"jdtls",
+	}
+}
+
 require('mason-lspconfig').setup_handlers {
   function(server_name)
     require('lspconfig')[server_name].setup {}
   end,
 }
+
+
