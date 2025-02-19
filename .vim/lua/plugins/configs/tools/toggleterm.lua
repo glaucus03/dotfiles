1123c3f27ec56b25cf257c42c595866768a4d480
glaucus03 <glaucus03.tech@gmail.com>
2025-02-19 09:10:12 +0900

    feat: add toggleterm settings :sparkles:

--- .vim/lua/plugins/configs/tools/toggleterm.lua
+++ .vim/lua/plugins/configs/tools/toggleterm.lua
@@ -4,8 +4,20 @@ return {
     'akinsho/toggleterm.nvim',
     cond = not env.is_vscode(),
     config = function()
-    
-  require("toggleterm").setup()
+      require("toggleterm").setup({
+        open_mapping = [[<leader>ft]],
+        size = 100,
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = 'float',
+        close_on_exit = true,
+      }
+      )
     end,
     doc = "ターミナル"
   },
