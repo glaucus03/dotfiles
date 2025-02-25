c1f19293868e20a7f91c5a0c9216dd0b5fce37c2
glaucus03 <glaucus03.tech@gmail.com>
2025-02-25 21:41:49 +0900

    fix: fix lazygit esc :+1:

--- .vim/lua/plugins/configs/tools/git.lua
+++ .vim/lua/plugins/configs/tools/git.lua
@@ -5,8 +5,31 @@ return {
     cond = not env.is_vscode(),
     dependencies = { 'nvim-telescope/telescope.nvim' },
     config = function()
-    
-  require('telescope').load_extension('lazygit')
+      require('telescope').load_extension('lazygit')
+      local group = vim.api.nvim_create_augroup("LazygitMods", { clear = true })
+      vim.api.nvim_create_autocmd("TermEnter", {
+        pattern = "*",
+        group = group,
+        callback = function()
+          local name = vim.api.nvim_buf_get_name(0)
+          if string.find(name, "lazygit") then
+            vim.keymap.set("t", "<ESC>",
+              function()
+                -- Get the terminal job ID for the current buffer
+                local bufnr = vim.api.nvim_get_current_buf()
+                local chan = vim.b[bufnr].terminal_job_id
+                if chan then
+                  -- Send the ESC key sequence to the terminal
+                  -- "\x1b" is the escape character
+                  vim.api.nvim_chan_send(chan, "\x1b")
+                end
+                --vim.cmd([[call feedkeys("q")]])
+              end,
+              { buffer = true })
+            return
+          end
+        end,
+      })
     end,
     doc = "Git UI"
   },
