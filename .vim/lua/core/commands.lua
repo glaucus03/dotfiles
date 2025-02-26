be52a54eb33d82a98676e767ebe5fc35be5ccd21
glaucus03 <glaucus03.tech@gmail.com>
2025-02-26 20:22:49 +0900

    feat: add close commands :sparkles:

--- .vim/lua/core/commands.lua
+++ .vim/lua/core/commands.lua
@@ -22,6 +22,29 @@ function M.setup()
   vim.api.nvim_create_user_command('CmReplaceCrlfToLf', function(opts)
     vim.cmd([[%s/\r//ge]])
   end, {})
+
+  vim.api.nvim_create_user_command('CloseBuffers', function()
+    local current_buf = vim.api.nvim_get_current_buf()
+    local buffers_to_keep = {}
+    local closed_count = 0
+
+    -- 現在のバッファと変更のあるバッファをリストに追加
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if vim.api.nvim_buf_get_option(buf, 'modified') or buf == current_buf then
+        table.insert(buffers_to_keep, buf)
+      end
+    end
+
+    -- 保持するバッファ以外を閉じる
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      if not vim.tbl_contains(buffers_to_keep, buf) then
+        if pcall(vim.api.nvim_buf_delete, buf, { force = false }) then
+          closed_count = closed_count + 1
+        end
+      end
+    end
+    vim.notify(closed_count .. " buffers closed", vim.log.levels.INFO)
+  end, {})
 end
 
 M.setup()
